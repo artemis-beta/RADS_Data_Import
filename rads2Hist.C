905f2a9ebbea16a35052ff5e6fb182fbb921c1c4
artemis-beta <KXZ186@student.bham.ac.uk>
2016-06-22 18:39:51 +0100

    Import Data as TTree added

--- rads2Hist.C
+++ rads2Hist.C
@@ -1,4 +1,15 @@
 void rads2Hist()
 {
-	// This is the code
+	TFile * output_file = new TFile("RADS_Data.root","UPDATE");
+
+	TH1D * hist = new TH1D("hist","Radiation Dosimetry with Altitude",100,0,10000);
+	
+	TTree * t = new TTree("t","RADS_Data");
+
+	t->ReadFile("RADS_long.dat","RAD_cpm/I");
+	
+	t->Write();
+	
+	output_file->Close();
+		
 }
